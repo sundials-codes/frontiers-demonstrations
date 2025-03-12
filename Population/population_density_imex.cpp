5e6eab2ce0ff44fb44d413b67a6a5cada4ff03f0
Sylvia Amihere <samihere@crimson.ua.edu>
2025-03-12 15:57:43 -0500

    Add files via upload

--- Population/population_density_imex.cpp
+++ Population/population_density_imex.cpp
@@ -1,6 +1,5 @@
 /*---------------------------------------------------------------
- * Programmer(s): Daniel R. Reynolds @ SMU
- * Modified by Sylvia Amihere @ SMU
+ * Programmer(s): Daniel R. Reynolds and Sylvia Amihere @ SMU
  *---------------------------------------------------------------
  * SUNDIALS Copyright Start
  * Copyright (c) 2002-2024, Lawrence Livermore National Security
@@ -81,19 +80,33 @@ using namespace std;
 #define ZERO SUN_RCONST(0.0)
 
 /* user data structure */
-typedef struct
-{
-  sunindextype N; /* number of intervals   */
-  sunrealtype dx; /* mesh spacing          */
-  sunrealtype k;  /* diffusion coefficient */
-}* UserData;
+// typedef struct
+// {
+//   sunindextype N; /* number of intervals   */
+//   sunrealtype dx; /* mesh spacing          */
+//   sunrealtype k;  /* diffusion coefficient */
+// }* UserData;
 
- class ARKODEParameters
- {
- public:
+  class UserData
+  {
+  public:
+    sunindextype N; /* number of intervals   */
+    sunrealtype dx; /* mesh spacing          */
+    sunrealtype k;  /* diffusion coefficient */
+
+  // constructor (with default values)
+  UserData()
+  : N(201),
+    k(0.02),
+    dx(ZERO){};
+  };
+
+class ARKODEParameters
+{
+public:
    // Integration method
-   std::string Iintegrator;
-   std::string Eintegrator;
+   std::string IMintegrator;
+   std::string EXintegrator;
  
    // Relative and absolute tolerances
    sunrealtype rtol;
@@ -104,33 +117,38 @@ typedef struct
  
    // Maximum number of time steps between outputs
    int maxsteps;
- 
+
+   // Time Parameters
+   sunrealtype T0;           // initial time
+   sunrealtype Tf;           // end time
+   int Nt;           // number of output times
+   
    // Output-related information
    int output;         // 0 = none, 1 = stats, 2 = disk, 3 = disk with tstop
-   int nout;           // number of output times
-   std::ofstream uout; // output file stream
+  std::ofstream uout; // output file stream
  
    // constructor (with default values)
-   ARKODEParameters()
-     : Iintegrator("ARKODE_LSRK_SSP_S_2"),
-       Eintegrator("ARKODE_LSRK_SSP_S_2"),
-       rtol(SUN_RCONST(1.e-4)),
-       atol(SUN_RCONST(1.e-11)),
-       fixed_h(ZERO),
-       maxsteps(10000),
-       output(1),
-       nout(10){};
+  ARKODEParameters()
+    : IMintegrator("ARKODE_SSP_SDIRK_2_1_2"),
+      EXintegrator("ARKODE_SSP_ERK_2_1_2"),
+      rtol(SUN_RCONST(1.e-4)),
+      atol(SUN_RCONST(1.e-10)),
+      fixed_h(ZERO),
+      maxsteps(10000),
+      output(1),
+      Nt(10),
+      T0(ZERO),
+      Tf(1.0){};
  
  }; // end ARKODEParameters
 
 /* User-supplied Functions Called by the Solver */
 static int fe(sunrealtype t, N_Vector y, N_Vector ydot, void* user_data); //Explicit RHS
 static int f(sunrealtype t, N_Vector y, N_Vector ydot, void* user_data); //Implicit RHS
-static int Jac(N_Vector v, N_Vector Jv, sunrealtype t, N_Vector y, N_Vector fy,
-               void* user_data, N_Vector tmp);
-static int ReadInputs(std::vector<std::string>& args, UserData& udata,
-  ARKODEParameters& uopts, SUNContext ctx);
+static int Jac(N_Vector v, N_Vector Jv, sunrealtype t, N_Vector y, N_Vector fy, void* user_data, N_Vector tmp);
+static int ReadInputs(std::vector<std::string>& args, UserData& udata, ARKODEParameters& uopts, SUNContext ctx);
 static void InputHelp();
+static int PrintSetup(UserData& udata,ARKODEParameters& uopts);
 
 /* Private function to check function return values */
 static int check_flag(void* flagvalue, const char* funcname, int opt);
@@ -152,50 +170,58 @@ int main(int argc, char* argv[])
   if (check_flag(&flag, "ReadInputs", 1)) { return 1; }
   if (flag > 0) { return 0; }
 
+
+
   /* general problem parameters */
-  const sunrealtype T0 = SUN_RCONST(0.0); /* initial time */
-  const sunrealtype Tf = SUN_RCONST(1.0); /* final time */
-  const int Nt         = 10;              /* total number of output times */
+  // const sunrealtype T0 = SUN_RCONST(0.0); /* initial time */
+  // const sunrealtype Tf = SUN_RCONST(1.0); /* final time */
+  // const int Nt         = 100;              /* total number of output times */
   // const sunrealtype rtol = 1.e-6;         /* relative tolerance */
   // const sunrealtype atol = 1.e-10;        /* absolute tolerance */
-  const sunindextype N = 201;             /* spatial mesh size */
-  const sunrealtype k  = 0.02; // d = 0.02, 0.04 or 0 /* diffusion coefficient */
+  // const sunindextype N = 201; //udata->N;             /* spatial mesh size */
+  // const sunrealtype k  = 0.02; //udata->k;//0.02; // d = 0.02, 0.04 or 0 /* diffusion coefficient */
 
   /* fill udata structure */
-  udata->N  = N;
-  udata->k  = k;
-  udata->dx = SUN_RCONST(1.0) / (N - 1); /* mesh spacing */
+  // UserData udata = (UserData)malloc(sizeof(*udata));
+  // udata->N  = N;
+  // udata->k  = k;
+  // udata->dx = SUN_RCONST(1.0) / (N - 1); /* mesh spacing */
 
   /* Initial problem output */
   printf("\n1D Population Density test problem:\n");
-  printf("  N = %li\n", (long int)udata->N);
-  printf("  diffusion coefficient:  k = %" GSYM "\n", udata->k);
+  printf("  N = %li\n", (long int)udata.N);
+  printf("  diffusion coefficient:  k = %" GSYM "\n", udata.k);
+
+  flag = PrintSetup(udata, uopts);
+  if (check_flag(&flag, "PrintSetup", 1)) { return 1;}
+  
 
   /* Initialize data structures */
-  N_Vector y = N_VNew_Serial(N, ctx); /* Create serial vector for solution */
+  N_Vector y = N_VNew_Serial(udata.N, ctx); /* Create serial vector for solution */
   if (check_flag((void*)y, "N_VNew_Serial", 0)) { return 1; }
   N_VConst(0.0, y); /* Set initial conditions */
 
   /* Call ARKStepCreate to initialize the ARK timestepper module and
      specify the right-hand side function in y'=f(t,y), the initial time
      T0, and the initial dependent variable vector y. */
-  void* arkode_mem = ARKStepCreate(fe, f, T0, y, ctx);
+  void* arkode_mem = ARKStepCreate(fe, f, uopts.T0, y, ctx);
   if (check_flag((void*)arkode_mem, "ARKStepCreate", 0)) { return 1; }
 
   /* Set routines */
-  flag = ARKodeSetUserData(arkode_mem, (void*)udata);
+  flag = ARKodeSetUserData(arkode_mem, &udata);
   if (check_flag(&flag, "ARKodeSetUserData", 1)) { return 1; }
-  flag = ARKodeSetMaxNumSteps(arkode_mem, 10000);
+  flag = ARKodeSetMaxNumSteps(arkode_mem, uopts.maxsteps);
   if (check_flag(&flag, "ARKodeSetMaxNumSteps", 1)) { return 1; }
   flag = ARKodeSStolerances(arkode_mem, uopts.rtol, uopts.atol);
   if (check_flag(&flag, "ARKodeSStolerances", 1)) { return 1; }
-  flag = ARKStepSetTableName(arkode_mem, "ARKODE_SSP_ESDIRK_4_2_3", "ARKODE_SSP_ERK_4_2_3"); //Sylvia: new embedded imex-ssp methods
+  flag = ARKStepSetTableName(arkode_mem, uopts.IMintegrator.c_str(), uopts.EXintegrator.c_str()); //Sylvia: new embedded imex-ssp methods
   if (check_flag(&flag, "ARKStepSetTableName", 1)) { return 1; } //Sylvia
   // flag = ARKStepWriteParameters(arkode_mem, stdout); //Sylvia
   // if (check_flag(&flag, "ARKStepWriteParameters", 1)) { return 1; } //Sylvia
+  
 
   /* Initialize PCG solver -- no preconditioning, with up to N iterations  */
-  SUNLinearSolver LS = SUNLinSol_PCG(y, 0, (int)N, ctx);
+  SUNLinearSolver LS = SUNLinSol_PCG(y, 0, udata.N, ctx);
   if (check_flag((void*)LS, "SUNLinSol_PCG", 0)) { return 1; }
 
   /* Linear solver interface */
@@ -204,7 +230,7 @@ int main(int argc, char* argv[])
 
   /* output mesh to disk */
   FILE* FID = fopen("population_mesh.txt", "w");
-  for (int i = 0; i < N; i++) { fprintf(FID, "  %.16" ESYM "\n", udata->dx * i); }
+  for (int i = 0; i < udata.N; i++) { fprintf(FID, "  %.16" ESYM "\n", udata.dx * i); }
   fclose(FID);
 
   /* Open output stream for results, access data array */
@@ -212,27 +238,27 @@ int main(int argc, char* argv[])
   sunrealtype* data = N_VGetArrayPointer(y);
 
   /* output initial condition to disk */
-  for (int i = 0; i < N; i++) { fprintf(UFID, " %.16" ESYM "", data[i]); }
+  for (int i = 0; i < udata.N; i++) { fprintf(UFID, " %.16" ESYM "", data[i]); }
   fprintf(UFID, "\n");
 
   /* Main time-stepping loop: calls ARKodeEvolve to perform the integration, then
      prints results.  Stops when the final time has been reached */
-  sunrealtype t = T0;
-  sunrealtype dTout = (Tf - T0) / Nt;
-  sunrealtype tout  = T0 + dTout;
+  sunrealtype t = uopts.T0;
+  sunrealtype dTout = (uopts.Tf - uopts.T0) / uopts.Nt;
+  sunrealtype tout  = uopts.T0 + dTout;
   printf("        t      ||u||_rms\n");
   printf("   -------------------------\n");
-  printf("  %10.6" FSYM "  %10.6f\n", t, sqrt(N_VDotProd(y, y) / N));
-  for (int iout = 0; iout < Nt; iout++)
+  printf("  %10.6" FSYM "  %10.6f\n", t, sqrt(N_VDotProd(y, y) / udata.N));
+  for (int iout = 0; iout < uopts.Nt; iout++)
   {
     flag = ARKodeEvolve(arkode_mem, tout, y, &t, ARK_NORMAL); /* call integrator */
     if (check_flag(&flag, "ARKodeEvolve", 1)) { break; }
     printf("  %10.6" FSYM "  %10.6f\n", t,
-           sqrt(N_VDotProd(y, y) / N)); /* print solution stats */
+           sqrt(N_VDotProd(y, y) / udata.N)); /* print solution stats */
     if (flag >= 0)
     { /* successful solve: update output time */
       tout += dTout;
-      tout = (tout > Tf) ? Tf : tout;
+      tout = (tout > uopts.Tf) ? uopts.Tf : tout;
     }
     else
     { /* unsuccessful solve: break */
@@ -241,7 +267,7 @@ int main(int argc, char* argv[])
     }
 
     /* output results to disk */
-    for (int i = 0; i < N; i++) { fprintf(UFID, " %.16" ESYM "", data[i]); }
+    for (int i = 0; i < udata.N; i++) { fprintf(UFID, " %.16" ESYM "", data[i]); }
     fprintf(UFID, "\n");
   }
   printf("   -------------------------\n");
@@ -253,7 +279,7 @@ int main(int argc, char* argv[])
 
   /* Clean up and return with successful completion */
   N_VDestroy(y);           /* Free vectors */
-  free(udata);             /* Free user data */
+  // free(udata);             /* Free user data */
   ARKodeFree(&arkode_mem); /* Free integrator memory */
   SUNLinSolFree(LS);       /* Free linear solver */
 
@@ -267,7 +293,7 @@ int main(int argc, char* argv[])
 /* f routine to compute the ODE explicitRHS function f(t,y). */
 static int fe(sunrealtype t, N_Vector y, N_Vector ydot, void* user_data)
 {
-  UserData udata = (UserData)user_data; /* access problem data */
+  UserData* udata = (UserData*)user_data; /* access problem data */
   sunrealtype *Y = NULL, *Ydot = NULL;
   Y = N_VGetArrayPointer(y); /* access data arrays */
   if (check_flag((void*)Y, "N_VGetArrayPointer", 0)) { return 1; }
@@ -303,6 +329,48 @@ static int fe(sunrealtype t, N_Vector y, N_Vector ydot, void* user_data)
 
   return 0; /* Return with success */
 }
+
+
+/* f routine to compute the ODE implicit RHS function f(t,y). */
+static int f(sunrealtype t, N_Vector y, N_Vector ydot, void* user_data)
+{
+  UserData* udata = (UserData*)user_data; /* access problem data */
+  sunrealtype *Y = NULL, *Ydot = NULL;
+  Y = N_VGetArrayPointer(y); /* access data arrays */
+  if (check_flag((void*)Y, "N_VGetArrayPointer", 0)) { return 1; }
+  Ydot = N_VGetArrayPointer(ydot);
+  if (check_flag((void*)Ydot, "N_VGetArrayPointer", 0)) { return 1; }
+  N_VConst(0.0, ydot); /* Initialize ydot to zero */
+
+  /* set parameters */
+  const sunindextype N = udata->N;
+  const sunrealtype k  = udata->k;
+  const sunrealtype dx = udata->dx;
+  const sunrealtype c1 = k / dx / dx;
+  const sunrealtype c2 = -2.0 * k / dx / dx;
+
+  /* update random seed */
+  srand(time(NULL));
+
+  /* iterate over domain, computing all equations */
+  if (t == 0.0) {
+    for (int i = 0; i < N; i++) {
+      sunrealtype Yleft = (i > 0) ? Y[i - 1] : Y[N - 1];
+      sunrealtype Yright = (i < N - 1) ? Y[i + 1] : Y[0];
+      Ydot[i] = c1 * Yleft + c2 * Y[i] + c1 * Yright;
+    }
+  } else {
+    for (int i = 0; i < N; i++) {
+      // sunrealtype rb = (dx*i <= 0.5) ? bRateE : 100.0*bRateE;
+      sunrealtype Yleft = (i > 0) ? Y[i - 1] : Y[N - 1];
+      sunrealtype Yright = (i < N - 1) ? Y[i + 1] : Y[0];
+      Ydot[i] = c1 * Yleft + c2 * Y[i] + c1 * Yright;
+    }
+  }
+
+  return 0; /* Return with success */
+}
+
 
 inline void find_arg(std::vector<std::string>& args, const std::string key,
   sunrealtype& dest)
@@ -377,101 +445,81 @@ static int ReadInputs(std::vector<std::string>& args, UserData& udata,
   }
 
 // Problem parameters
-//  find_arg(args, "--gamma", udata.gamma);
-//  find_arg(args, "--tf", udata.tf);
-//  find_arg(args, "--xl", udata.xl);
-//  find_arg(args, "--xr", udata.xr);
-//  find_arg(args, "--nx", udata.nx);
+ find_arg(args, "--N", udata.N);
+ find_arg(args, "--k", udata.k);
 
 // Integrator options
-//  find_arg(args, "--integrator", uopts.integrator);
-//  find_arg(args, "--stages", uopts.stages);
+  find_arg(args, "--IMintegrator", uopts.IMintegrator);
+  find_arg(args, "--EXintegrator", uopts.EXintegrator);
   find_arg(args, "--rtol", uopts.rtol);
   find_arg(args, "--atol", uopts.atol);
-//  find_arg(args, "--fixed_h", uopts.fixed_h);
-//  find_arg(args, "--maxsteps", uopts.maxsteps);
-//  find_arg(args, "--output", uopts.output);
-//  find_arg(args, "--nout", uopts.nout);
+ find_arg(args,  "--fixed_h", uopts.fixed_h);
+ find_arg(args,  "--maxsteps", uopts.maxsteps);
+ find_arg(args,  "--output", uopts.output);
+ find_arg(args,  "--T0", uopts.T0);
+ find_arg(args,  "--Tf", uopts.Tf);
+ find_arg(args,  "--Nt", uopts.Nt);
 
-// Recompute mesh spacing and [re]allocate flux array
+ // Recompute mesh spacing and [re]allocate flux array
+ udata.dx = SUN_RCONST(1.0) / (udata.N - 1);
 //  udata.dx = (udata.xr - udata.xl) / ((sunrealtype)udata.nx);
-//  if (udata.flux) { delete[] udata.flux; }
-//  udata.flux = new sunrealtype[NSPECIES * (udata.nx + 1)];
-
-//  if (uopts.stages < 0)
-//  {
-//    std::cerr << "ERROR: Invalid number of stages" << std::endl;
-//    return -1;
-//  }
 
 return 0;
 }
-
 
 
 static void InputHelp()
  {
    std::cout << std::endl;
    std::cout << "Command line options:" << std::endl;
-  //  std::cout << "  --integrator <str> : method (ARKODE_LSRK_SSP_S_2, "
-  //               "ARKODE_LSRK_SSP_S_3, "
-  //               "ARKODE_LSRK_SSP_10_4, or any valid ARKODE_ERKTableID)\n";
-  //  std::cout << "  --stages <int>     : number of stages (ignored for "
-  //               "ARKODE_LSRK_SSP_10_4 and ERK)\n";
-  //  std::cout << "  --tf <real>        : final time\n";
-  //  std::cout << "  --xl <real>        : domain lower boundary\n";
-  //  std::cout << "  --xr <real>        : domain upper boundary\n";
-  //  std::cout << "  --gamma <real>     : ideal gas constant\n";
-  //  std::cout << "  --nx <int>         : number of mesh points\n";
-   std::cout << "  --rtol <real>      : relative tolerance\n";
-   std::cout << "  --atol <real>      : absolute tolerance\n";
-  //  std::cout << "  --fixed_h <real>   : fixed step size\n";
-  //  std::cout << "  --maxsteps <int>   : max steps between outputs\n";
-  //  std::cout << "  --output <int>     : output level\n";
-  //  std::cout << "  --nout <int>       : number of outputs\n";
-   std::cout << "  --help             : print options and exit\n";
+   std::cout << "  --IMintegrator <str> : method (ARKODE_SSP_SDIRK_2_1_2, "
+                "ARKODE_SSP_DIRK_3_1_2, " 
+                "ARKODE_SSP_LSPUM_SDIRK_3_1_2, or ARKODE_SSP_ESDIRK_4_2_3)\n";
+   std::cout << "  --EXintegrator <str> : method (ARKODE_SSP_ERK_2_1_2, "
+                "ARKODE_SSP_ERK_3_1_2, " 
+                "ARKODE_SSP_LSPUM_ERK_3_1_2, or ARKODE_SSP_ERK_4_2_3)\n";
+   std::cout << "  --N <int>         : dimension\n";
+   std::cout << "  --k <real>        : diffusion coefficient: (0.0, 0.02 or 0.04)\n";
+   std::cout << "  --rtol <real>     : relative tolerance\n";
+   std::cout << "  --atol <real>     : absolute tolerance\n";
+   std::cout << "  --fixed_h <real>  : fixed step size\n";
+   std::cout << "  --maxsteps <int>  : max steps between outputs\n";
+   std::cout << "  --output <int>    : output level\n";
+   std::cout << "  --T0 <real>        : initial time \n";
+   std::cout << "  --Tf <real>        : end time\n";
+   std::cout << "  --Nt <int>        : number of outputs\n";
+   std::cout << "  --help            : print options and exit\n";
  }
 
 
-/* f routine to compute the ODE implicit RHS function f(t,y). */
-static int f(sunrealtype t, N_Vector y, N_Vector ydot, void* user_data)
+ // Print user data
+static int PrintSetup(UserData& udata, ARKODEParameters& uopts)
 {
-  UserData udata = (UserData)user_data; /* access problem data */
-  sunrealtype *Y = NULL, *Ydot = NULL;
-  Y = N_VGetArrayPointer(y); /* access data arrays */
-  if (check_flag((void*)Y, "N_VGetArrayPointer", 0)) { return 1; }
-  Ydot = N_VGetArrayPointer(ydot);
-  if (check_flag((void*)Ydot, "N_VGetArrayPointer", 0)) { return 1; }
-  N_VConst(0.0, ydot); /* Initialize ydot to zero */
+  std::cout << std::endl;
+  std::cout << "Problem parameters and options:" << std::endl;
+  std::cout << " --------------------------------- " << std::endl;
+  std::cout << "  N            = " << udata.N << std::endl;
+  std::cout << "  k            = " << udata.k << std::endl;
+  std::cout << "  dx           = " << udata.dx << std::endl;
+  std::cout << " --------------------------------- " << std::endl;
+  std::cout << "  IMintegrator = " << uopts.IMintegrator << std::endl;
+  std::cout << "  EXintegrator = " << uopts.EXintegrator << std::endl;
+  std::cout << "  rtol         = " << uopts.rtol << std::endl;
+  std::cout << "  atol         = " << uopts.atol << std::endl;
+  std::cout << "  fixed h      = " << uopts.fixed_h << std::endl;
+  std::cout << "  maxsteps     = " << uopts.maxsteps << std::endl;
+  std::cout << "  T0           = " << uopts.T0 << std::endl;
+  std::cout << "  Tf           = " << uopts.Tf << std::endl;
+  std::cout << "  Nt           = " << uopts.Nt << std::endl;
+  std::cout << " --------------------------------- " << std::endl;
+  std::cout << "  output       = " << uopts.output << std::endl;
+  std::cout << " --------------------------------- " << std::endl;
+  std::cout << std::endl;
 
-  /* set parameters */
-  const sunindextype N = udata->N;
-  const sunrealtype k  = udata->k;
-  const sunrealtype dx = udata->dx;
-  const sunrealtype c1 = k / dx / dx;
-  const sunrealtype c2 = -2.0 * k / dx / dx;
-
-  /* update random seed */
-  srand(time(NULL));
-
-  /* iterate over domain, computing all equations */
-  if (t == 0.0) {
-    for (int i = 0; i < N; i++) {
-      sunrealtype Yleft = (i > 0) ? Y[i - 1] : Y[N - 1];
-      sunrealtype Yright = (i < N - 1) ? Y[i + 1] : Y[0];
-      Ydot[i] = c1 * Yleft + c2 * Y[i] + c1 * Yright;
-    }
-  } else {
-    for (int i = 0; i < N; i++) {
-      // sunrealtype rb = (dx*i <= 0.5) ? bRateE : 100.0*bRateE;
-      sunrealtype Yleft = (i > 0) ? Y[i - 1] : Y[N - 1];
-      sunrealtype Yright = (i < N - 1) ? Y[i + 1] : Y[0];
-      Ydot[i] = c1 * Yleft + c2 * Y[i] + c1 * Yright;
-    }
-  }
-
-  return 0; /* Return with success */
+  return 0;
 }
+ 
+
 
 /*-------------------------------
  * Private helper functions
