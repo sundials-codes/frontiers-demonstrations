a5fa8d6ea63bc6c81e6bdb60c74e2a5b34299f04
Sylvia Amihere <samihere@login17.chn.perlmutter.nersc.gov>
2026-01-05 08:21:47 -0800

    minor changes to hyperbolic relaxation

--- hyperbolic_relaxation/run-all-hyperbolic-relaxation-tests.sh
+++ hyperbolic_relaxation/run-all-hyperbolic-relaxation-tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # This script generates all results for the hyperbolic relaxation problem.
 
-echo -e "Generating reference solutions for each stiffness paramater\n"
+echo -e "Generating reference solution for each stiffness paramater\n"
 python3 runtests_hyperbolic_relaxation_refSol.py
 
-echo -e "Generating all stats and saving in an excel file\n"
+echo -e "Generating all stats and saving in an excel file as well as plots\n"
 python3 runtests_hyperbolic_relaxation.py
 
 echo -e "Run tests completed!\n"
