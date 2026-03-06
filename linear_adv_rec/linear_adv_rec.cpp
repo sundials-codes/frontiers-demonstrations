d1731c3b7d3d30d91375ef0d2508d0eba8a24269
Sylvia Amihere <samihere@crimson.ua.edu>
2026-03-06 09:30:07 -0500

    finalized example for linear advection reaction

--- linear_adv_rec/linear_adv_rec.cpp
+++ linear_adv_rec/linear_adv_rec.cpp
@@ -389,7 +389,7 @@ static int fe(sunrealtype t, N_Vector y, N_Vector ydot, void* user_data)
   // left boundary
   sunrealtype pi = 3.1415926535;
   sunrealtype expo = -30000.0 * (t - 0.5) * (t - 0.5);
-  u[0]    = 1.0 + 0.2 * sin(10.0 * pi * t) + 1.0 * sin (0.5 * exp(expo)) ;
+  u[0]    = 1.0 + 0.2 * sin(10.0 * pi * t) + 1.5 * sin (10.0 * pi * t) * exp(expo) ;
 
   udot[0] = -alpha1 * (-11.0 * u[0] + 18.0 * u[1] - 9.0  * u[2] + 2.0 * u[3]       ) / (6.0  * dx);
   udot[1] = -alpha1 * (-3.0  * u[0] - 10.0 * u[1] + 18.0 * u[2] - 6.0 * u[3] + u[4]) / (12.0 * dx);
@@ -432,7 +432,7 @@ static int f(sunrealtype t, N_Vector y, N_Vector ydot, void* user_data)
   //boundary conditions
   sunrealtype pi = 3.1415926535;
   sunrealtype expo = -30000.0 * (t - 0.5) * (t - 0.5);
-  u[0]    = 1.0 + 0.2 * sin(10.0 * pi * t) + 1.0 * sin (0.5 * exp(expo)) ;
+  u[0]    = 1.0 + 0.2 * sin(10.0 * pi * t) + 1.5 * sin (10.0 * pi * t) * exp(expo) ;
 
   //interior points
   for (int i = 0; i < N; i++){
--- linear_adv_rec/linear_adv_rec_refSol.cpp
+++ linear_adv_rec/linear_adv_rec_refSol.cpp
@@ -393,7 +393,7 @@ static int f(sunrealtype t, N_Vector y, N_Vector ydot, void* user_data)
   // left boundary
   sunrealtype pi = 3.1415926535;
   sunrealtype expo = -30000.0 * (t - 0.5) * (t - 0.5);
-  u[0]    = 1.0 + 0.2 * sin(10.0 * pi * t) + 1.0 * sin (0.5 * exp(expo)) ;
+  u[0]    = 1.0 + 0.2 * sin(10.0 * pi * t) + 1.5 * sin (10.0 * pi * t) * exp(expo) ;
 
   udot[0] = (-alpha1 * (-11.0 * u[0] + 18.0 * u[1] - 9.0  * u[2] + 2.0 * u[3]       ) / (6.0  * dx))
             -k1*u[0] + k2*v[0] + s1;
